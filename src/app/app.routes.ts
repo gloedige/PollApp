68ab2ce65cd89277a5b142b00cb37375a0ad1f86
gloedige <gerd.loedige@posteo.de>
2026-05-27 14:54:37 +0200

    add dashboard to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SurveyDashboard } from './dashboard/survey-dashboard/survey-dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: SurveyDashboard}
+];
