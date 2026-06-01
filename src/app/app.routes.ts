3632c678a16f0af24282fc9812f4af401520a2f4
gloedige <gerd.loedige@posteo.de>
2026-06-01 21:50:21 +0200

    create first elements on detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,5 @@ import { SurveyDetail } from './features/survey-detail/survey-detail';
 export const routes: Routes = [
     {path: "", redirectTo: "dashboard", pathMatch: "full"},
     {path: "dashboard", component: SurveyDashboard},
-    {path: "detail/:id", component: SurveyDetail}
+    {path: "detail", component: SurveyDetail}
 ];
