937e46800d7019e9f3d5c5d2f7003d17d99e9e52
gloedige <gerd.loedige@posteo.de>
2026-06-18 14:27:27 +0200

    update js doc

--- src/app/features/components/question-option-block/question-option-block.ts
+++ src/app/features/components/question-option-block/question-option-block.ts
@@ -14,17 +14,18 @@ import { SurveyService } from '../../services/survey-service';
 
 export class QuestionOptionBlock {
   questionId = input<number>(0);
+  selectedOptionIds = signal<number[]>([]);
   
   surveyDetails = inject(SurveyDetail);
   dbService = inject(SupabaseService);
   readonly surveyService = inject(SurveyService);
   readonly questionOptions = computed(() => this.dbService.options().filter(option => option.question_id === this.questionId()));
-  readonly isSingleOptionSelected = signal<boolean>(false);
+  readonly isSingleOptionSelected = signal<boolean>(false); // not in use yet!
   readonly hasMultipleOptions = signal<boolean>(false);
   readonly questionText = signal('');
   readonly numberOfQuestion = signal(0);
   readonly order_letter = signal<string[]>(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']);
-
+  
 
 
   /**
@@ -42,31 +43,67 @@ export class QuestionOptionBlock {
   }
 
 
-  selectedOptionIds = signal<number[]>([]);
 
+  /**
+   * This function checks if a specific option ID is currently selected. It does this by checking if the option ID is included in the 
+   * selectedOptionIds signal.
+   * @param id -  The ID of the option to check for selection.
+   * @returns - A boolean value indicating whether the specified option ID is currently selected or not.
+   */
   isSelected(id: number): boolean {
     return this.selectedOptionIds().includes(id);
   }
 
-
+  /**
+   * This function handles the toggle event of a checkbox for an option. It checks if the question allows multiple options and updates the
+   * selectedOptionIds signal accordingly. If multiple options are allowed, it adds or removes the option ID from the selectedOptionIds array.
+   * If only a single option is allowed, it sets the selectedOptionIds signal to an array containing only the toggled option ID or an empty array 
+   * if the same option is toggled again.
+   * @param optionId - The ID of the option that was toggled. (not in use yet, but will be needed for the backend implementation)
+   */
   handleCheckboxToggle(optionId: number): void {
     const currentSelectedOptionIds = this.selectedOptionIds();
 
     if(this.hasMultipleOptions()) {
-      if (currentSelectedOptionIds.includes(optionId)) {
+      this.handleMultipleOptionsToggle(optionId, currentSelectedOptionIds);
+    } else {
+      this.handleSingleOptionToggle(optionId, currentSelectedOptionIds);
+    }
+  }
+
+  /**
+   * This function handles the toggle event for multiple options. It checks if the toggled option ID is already included in the currentSelectedOptionIds 
+   * array. If it is included, it removes the option ID from the selectedOptionIds array. If it is not included, it adds the option ID to the selectedOptionIds
+   * array.
+   * @param optionId - The ID of the option that was toggled.
+   * @param currentSelectedOptionIds - The array of currently selected option IDs.
+   */
+  private handleMultipleOptionsToggle(optionId: number, currentSelectedOptionIds: number[]): void {
+    if (currentSelectedOptionIds.includes(optionId)) {
         this.selectedOptionIds.set(currentSelectedOptionIds.filter(id => id !== optionId));
       } else {
         this.selectedOptionIds.set([...currentSelectedOptionIds, optionId]);
       } 
+  }
+
+  /**
+   * This function handles the toggle event for single options. It checks if the toggled option ID is already included in the currentSelectedOptionIds array.
+   * If it is included, it clears the selectedOptionIds signal. If it is not included, it sets the selectedOptionIds signal to an array containing only the 
+   * toggled option ID.
+   * @param optionId - The ID of the option that was toggled.
+   * @param currentSelectedOptionIds - The array of currently selected option IDs.
+   */
+  private handleSingleOptionToggle(optionId: number, currentSelectedOptionIds: number[]): void {
+    if (currentSelectedOptionIds.includes(optionId)) {
+      this.selectedOptionIds.set([]);
+      this.isSingleOptionSelected.set(false); // not in use yet!
     } else {
-      if (currentSelectedOptionIds.includes(optionId)) {
-        this.selectedOptionIds.set([]);
-      } else {
-        this.selectedOptionIds.set([optionId]);
-      }
+      this.selectedOptionIds.set([optionId]);
+      this.isSingleOptionSelected.set(true); // not in use yet!
     }
   }
-  
+
+
 }
 
 
